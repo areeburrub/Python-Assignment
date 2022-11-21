6ea8009e5efc2487bb8658a18c117b83d8debd22
areeburrub <areeburrub@gmail.com>
2022-11-21 13:25:01 +0530

    fixed: symmetric string

--- Assignment 1/05 - symmteric_string.py
+++ Assignment 1/05 - symmteric_string.py
@@ -1,7 +1,9 @@
 #Take string from user
 string = input("Enter a string: ")
 #Check if string is symmetric
-if string == string[::-1]:
+stringLength = (len(string))
+print(stringLength)
+if string[:int(stringLength/2)] == string[int(stringLength/2):]:
     print("String is symmetric")
 else:
     print("String is not symmetric")
